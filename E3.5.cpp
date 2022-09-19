e69080dd3892419bb06cf1b69c435003ffb8ec32
fmesa1213 <52666235+fmesa1213@users.noreply.github.com>
2022-09-19 19:15:23 -0400

    Update E3.5.cpp

--- E3.5.cpp
+++ E3.5.cpp
@@ -4,8 +4,7 @@ Course: CSCI-135
 Instructor: Genady Maryash
 Assignment: Homework E3.5
 I wrote a program that reads three numbers and prints “increasing” if they are in increasing order, “decreasing” if they are in decreasing order, 
-and “neither” otherwise. Here, “increasing” means “strictly increasing”, with each value larger than its predecessor. 
-The sequence 3 4 4 would not be considered increasing.
+and “neither” otherwise. 
 */
 
 #include <iostream>
